496eed4bdbef5ee3cd265bb3ecb742ae518a6cfc
unurgunite <senpaiguru1488@gmail.com>
2023-10-14 22:27:42 +0300

    Updated default specs

--- spec/code_generator_spec.rb
+++ spec/code_generator_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe CodeGenerator do
   it "has a version number" do
     expect(CodeGenerator::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
